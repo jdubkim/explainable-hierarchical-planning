c53127f28a546e743a269a4704838846d634e016
Jeewoo Kim <jk3417@aube.doc.ic.ac.uk>
2023-04-05 15:34:48 +0100

    Changes to requirements.txt, add bash scripts to train on GPU clusters (SLURM), add folder for slurm output files

---
+++ scripts/train_director.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=END
+#SBATCH --mail-user=jk3417
+export PATH=/vol/bitbucket/jk3417/xmbhrl/bin/:$PATH
+source activate
+source /vol/cuda/11.4.120-cudnn8.2.4/setup.sh
+TERM=vt100 # or TERM=xterm
+/usr/bin/nvidia-smi
+uptime
+
+echo "Using config: $1.”
+echo "Running task: $2.”
+srun python /vol/bitbucket/jk3417/explainable-mbhrl/embodied/agents/director/train.py --logdir /vol/bitbucket/jk3417/explainable-mbhrl/logdir/$(date +%Y%m%d-%H%M%S) --configs $1 --task $2
+
+# Param 1: config (e.g.  dmlab, atari, crafter, dmc_vision, dmc_proprio, pinpad, loconav)
+# Param 2: task (e.g. dmc_walker_walk)
