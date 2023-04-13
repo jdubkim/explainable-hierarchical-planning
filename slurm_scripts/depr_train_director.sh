164680b9973d21a60307b8bac91135503b22f0b4
Jeewoo Kim <jeewoo1998@gmail.com>
2023-04-13 17:32:44 +0100

    Fix train_director.sh - Now successfully catch GPU

---
+++ slurm_scripts/depr_train_director.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=END
+#SBATCH --mail-user=jk3417
+#SBATCH --output=/vol/bitbucket/jk3417/explainable-mbhrl/slurm_outputs/director_result_%j.out
+
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/vol/bitbucket/${USER}/xmbhrl/lib/python3.10/site-packages/tensorrt/
+export PATH=/vol/bitbucket/jk3417/xmbhrl/bin/:$PATH
+source activate
+
+readonly CUDA_VERSION=11.4.120-cudnn8.2.4
+echo "CUDA: $CUDA_VERSION"
+
+if [[ $(getconf LONG_BIT) == "32" ]]; then
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/vol/cuda/$CUDA_VERSION/lib
+    else
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/vol/cuda/$CUDA_VERSION/lib64:/vol/cuda/$CUDA_VERSION/lib
+fi
+
+if [ -f /vol/cuda/$CUDA_VERSION/setup.sh ]
+    then
+        . /vol/cuda/$CUDA_VERSION/setup.sh
+    else
+        echo "ERROR: /vol/cuda/$CUDA_VERSION/setup.sh not found"
+fi
+
+echo "CUDA version $(nvcc --version | grep 'release' | awk '{print $5}') is installed"
+dirname $(dirname $(which nvcc))
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=/vol/cuda/$CUDA_VERSION
+echo "$XLA_FLAGS"
+
+TERM=vt100 # or TERM=xterm
+/usr/bin/nvidia-smi
+uptime
+
+echo "Using config: $1."
+echo "Running task: $2."
+srun python /vol/bitbucket/jk3417/explainable-mbhrl/embodied/agents/director/train.py \
+  --logdir /vol/bitbucket/jk3417/explainable-mbhrl/logdir/$(date +%Y%m%d-%H%M%S) \
+  --configs $1 --task $2
+
+# Param 1: config (e.g.  dmlab, atari, crafter, dmc_vision, dmc_proprio, pinpad, loconav)
+# Param 2: task (e.g. dmc_walker_walk)
--- slurm_scripts/train_director.sh
+++ slurm_scripts/train_director.sh
@@ -6,34 +6,16 @@
 export PATH=/vol/bitbucket/jk3417/xmbhrl/bin/:$PATH
 source activate
 
-#use cuda 11.2
-if [[ $(getconf LONG_BIT) == "32" ]]; then
-        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/vol/cuda/11.2.1-cudnn8.1.0.77/lib
-    else
-        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/vol/cuda/11.2.1-cudnn8.1.0.77/lib64:/vol/cuda/11.2.1-cudnn8.1.0.77/lib
-fi
-
-if [ -f /vol/cuda/11.2.1-cudnn8.1.0.77/setup.sh ]
-    then
-        . /vol/cuda/11.2.1-cudnn8.1.0.77/setup.sh
-    else
-        echo "ERROR: /vol/cuda/11.2.1-cudnn8.1.0.77/setup.sh not found"
-fi
-
-echo "CUDA version $(nvcc --version | grep 'release' | awk '{print $5}') is installed"
-dirname $(dirname $(which nvcc))
-
-export XLA_FLAGS=--xla_gpu_cuda_data_dir=/vol/cuda/11.2.1-cudnn8.1.0.77
-
+. /vol/cuda/11.4.120-cudnn8.2.4/setup.sh
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=/vol/cuda/11.4.120-cudnn8.2.4
+python -c "import tensorflow as tf; print(tf.config.experimental.list_physical_devices(\"GPU\"))"
 TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
 uptime
 
 echo "Using config: $1."
 echo "Running task: $2."
-cd /vol/bitbucket/${USER}/explainable-mbhrl
-srun python embodied/agents/director/train.py --logdir ./logdir/$(date +%Y%m%d-%H%M%S) \\
-    --configs $1 --task $2
+srun python /vol/bitbucket/jk3417/explainable-mbhrl/embodied/agents/director/train.py --logdir /vol/bitbucket/jk3417/explainable-mbhrl/logdir/$(date +%Y%m%d-%H%M%S) --configs $1 --task $2
 
 # Param 1: config (e.g.  dmlab, atari, crafter, dmc_vision, dmc_proprio, pinpad, loconav)
 # Param 2: task (e.g. dmc_walker_walk)
