18f6324025502dc99bcf9f1ab905b32addce680f
jdubkim <jeewoo1998@gmail.com>
2023-05-04 15:59:32 +0100

    Add a debug folder to benchmark tensorflow performance.

---
+++ debug/tf_benchmark.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+import time
+import matplotlib.pyplot as plt
+
+#something of an extra or the below code will produce an error as "Tensor.graph is undefined when eager execution is enabled."
+#this code is needed to not let tensorflow produce error for the cpu part of code or the gpu part.
+#the reason for this error is because Session does not work with either eager execution or tf.function, and you should not invoke it directly.
+
+tf.compat.v1.disable_eager_execution()
+
+cpu_times = []
+sizes = [1, 10, 100, 500, 1000, 2000, 3000, 4000, 5000, 8000, 10000]
+for size in sizes:
+    tf.compat.v1.reset_default_graph()
+    start = time.time()
+    with tf.device('cpu:0'):
+        v1 = tf.Variable(tf.random.normal((size, size)))
+        v2 = tf.Variable(tf.random.normal((size, size)))
+        op = tf.matmul(v1, v2)
+
+    with tf.compat.v1.Session() as sess:
+        sess.run(tf.compat.v1.global_variables_initializer())
+        sess.run(op)
+    cpu_times.append(time.time() - start)
+    print('cpu time took: {0:.4f}'.format(time.time() - start))
+
+gpu_times = []
+for size in sizes:
+    tf.compat.v1.reset_default_graph()
+    start = time.time()
+    with tf.device('gpu:0'):
+        v1 = tf.Variable(tf.random.normal((size, size)))
+        v2 = tf.Variable(tf.random.normal((size, size)))
+        op = tf.matmul(v1, v2)
+
+    with tf.compat.v1.Session() as sess:
+        sess.run(tf.compat.v1.global_variables_initializer())
+        sess.run(op)
+    gpu_times.append(time.time() - start)
+    print('gpu time took: {0:.4f}'.format(time.time() - start))
+
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.plot(sizes, gpu_times, label='GPU')
+ax.plot(sizes, cpu_times, label='CPU')
+plt.xlabel('MATRIX SIZE')
+plt.ylabel('TIME (sec)')
+plt.legend()
+plt.show()
