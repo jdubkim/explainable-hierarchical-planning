033cd4d9cdf6cf162747457684f9ffde1e5910ce
jdubkim <jeewoo1998@gmail.com>
2023-06-17 14:25:48 +0100

    Log features needed to generate world graphs.

--- embodied/agents/director/train_with_viz.py
+++ embodied/agents/director/train_with_viz.py
@@ -1,72 +1,75 @@
 import collections
+import os
 import re
 import warnings
 
 import embodied
 import numpy as np
 
-# Train an agent with visualization.
-def train_with_viz(agent, env, train_replay, eval_replay, logger, args):
 
+# Train an agent with visualization.
+def train_with_viz(
+    agent, env, train_replay, eval_replay, logger, args, final_eval_replay=None
+):
   logdir = embodied.Path(args.logdir)
   logdir.mkdirs()
-  print('Logdir', logdir)
+  print("Logdir", logdir)
   should_train = embodied.when.Every(args.train_every)
   should_log = embodied.when.Every(args.log_every)
   should_expl = embodied.when.Until(args.expl_until)
   should_video = embodied.when.Every(args.eval_every)
   step = logger.step
+  eval_step = embodied.Counter()
 
   timer = embodied.Timer()
-  timer.wrap('agent', agent, ['policy', 'train', 'report', 'save'])
-  timer.wrap('env', env, ['step'])
-  if hasattr(train_replay, '_sample'):
-    timer.wrap('replay', train_replay, ['_sample'])
+  timer.wrap("agent", agent, ["policy", "train", "report", "save"])
+  timer.wrap("env", env, ["step"])
+  if hasattr(train_replay, "_sample"):
+    timer.wrap("replay", train_replay, ["_sample"])
 
   nonzeros = set()
-  
+
   def per_episode(ep):
     metrics = {}
-    length = len(ep['reward']) - 1
-    score = float(ep['reward'].astype(np.float64).sum())
-    print(f'Episode has {length} steps and return {score:.1f}.')
-    metrics['length'] = length
-    metrics['score'] = score
-    metrics['reward_rate'] = (ep['reward'] - ep['reward'].min() >= 0.1).mean()
+    length = len(ep["reward"]) - 1
+    score = float(ep["reward"].astype(np.float64).sum())
+    print(f"Episode has {length} steps and return {score:.1f}.")
+    metrics["length"] = length
+    metrics["score"] = score
+    metrics["reward_rate"] = (ep["reward"] - ep["reward"].min() >= 0.1).mean()
     logs = {}
     should_video_now = should_video(step)
     if not should_video_now:
-      ep.pop('full_render', None)
+      ep.pop("full_render", None)
     for key, value in ep.items():
       if not args.log_zeros and key not in nonzeros and (value == 0).all():
         continue
       nonzeros.add(key)
       if re.match(args.log_keys_sum, key):
-        logs[f'sum_{key}'] = ep[key].sum()
+        logs[f"sum_{key}"] = ep[key].sum()
       if re.match(args.log_keys_mean, key):
-        logs[f'mean_{key}'] = ep[key].mean()
+        logs[f"mean_{key}"] = ep[key].mean()
       if re.match(args.log_keys_max, key):
-        logs[f'max_{key}'] = ep[key].max(0).mean()
+        logs[f"max_{key}"] = ep[key].max(0).mean()
     # Record and add the video to the metrics every evaluation step.
     if should_video_now:
       for key in args.log_keys_video:
-        if key == 'none':
+        if key == "none":
           continue
-        metrics[f'policy_{key}'] = ep[key]
-        if 'log_goal' in ep:
-          if ep[key].shape == ep['log_goal'].shape:
-            goal = (255 * ep['log_goal']).astype(np.uint8)
-            metrics[f'policy_image_with_goal'] = np.concatenate(
-                [ep['image'], goal], 2)
-      
-    logger.add(metrics, prefix='episode')
-    logger.add(logs, prefix='logs')
-    logger.add(train_replay.stats, prefix='replay')
+        metrics[f"policy_{key}"] = ep[key]
+        if "log_goal" in ep:
+          if ep[key].shape == ep["log_goal"].shape:
+            goal = (255 * ep["log_goal"]).astype(np.uint8)
+            metrics[f"policy_image_with_goal"] = np.concatenate([ep["image"], goal], 2)
+
+    logger.add(metrics, prefix="episode")
+    logger.add(logs, prefix="logs")
+    logger.add(train_replay.stats, prefix="replay")
     logger.write()
 
   fill = max(0, args.eval_fill - len(eval_replay))
   if fill:
-    print(f'Fill eval dataset ({fill} steps).')
+    print(f"Fill eval dataset ({fill} steps).")
     eval_driver = embodied.Driver(env)
     eval_driver.on_step(eval_replay.add)
     random_agent = embodied.RandomAgent(env.act_space)
@@ -79,7 +82,7 @@ def train_with_viz(agent, env, train_replay, eval_replay, logger, args):
   driver.on_step(train_replay.add)
   fill = max(0, args.train_fill - len(train_replay))
   if fill:
-    print(f'Fill train dataset ({fill} steps).')
+    print(f"Fill train dataset ({fill} steps).")
     random_agent = embodied.RandomAgent(env.act_space)
     driver(random_agent.policy, steps=fill, episodes=1)
 
@@ -92,45 +95,66 @@ def train_with_viz(agent, env, train_replay, eval_replay, logger, args):
 
   metrics = collections.defaultdict(list)
   batch = [None]
+
   def train_step(tran, worker):
     if should_train(step):
       for _ in range(args.train_steps):
         batch[0] = next(dataset_train)
         outs, state[0], mets = agent.train(batch[0], state[0])
         [metrics[key].append(value) for key, value in mets.items()]
-        if 'priority' in outs:
-          train_replay.prioritize(outs['key'], outs['priority'])
+        if "priority" in outs:
+          train_replay.prioritize(outs["key"], outs["priority"])
     if should_log(step):
       with warnings.catch_warnings():  # Ignore empty slice warnings.
-        warnings.simplefilter('ignore', category=RuntimeWarning)
+        warnings.simplefilter("ignore", category=RuntimeWarning)
         for name, values in metrics.items():
-          logger.scalar('train/' + name, np.nanmean(values, dtype=np.float64))
+          logger.scalar("train/" + name, np.nanmean(values, dtype=np.float64))
           metrics[name].clear()
-      logger.add(agent.report(batch[0]), prefix='report')
-      logger.add(agent.report(next(dataset_eval)), prefix='eval')
-      logger.add(timer.stats(), prefix='timer')
+      logger.add(agent.report(batch[0]), prefix="report")
+      logger.add(agent.report(next(dataset_eval)), prefix="eval")
+      logger.add(timer.stats(), prefix="timer")
       logger.write(fps=True)
+
   driver.on_step(train_step)
 
-  checkpoint = embodied.Checkpoint(logdir / 'checkpoint.pkl')
+  checkpoint = embodied.Checkpoint(logdir / "checkpoint.pkl")
   checkpoint.step = step
   checkpoint.agent = agent
   checkpoint.train_replay = train_replay
   checkpoint.eval_replay = eval_replay
   checkpoint.load_or_save()
 
-  print('Start training loop.')
+  print("Start training loop.")
   policy = lambda *args: agent.policy(
-      *args, mode='explore' if should_expl(step) else 'train')
+      *args, mode="explore" if should_expl(step) else "train"
+  )
   while step < args.steps:
-    # scalars = collections.defaultdict(list)
-    # for _ in range(args.eval_samples):
-    #   for key, value in agent.report(next(dataset_eval)).items():
-    #     if value.shape == ():
-    #       scalars[key].append(value)
-    # for name, values in scalars.items():
-    #   logger.scalar(f'eval/{name}', np.array(values, np.float64).mean())
     logger.write()
     driver(policy, steps=args.eval_every)
     checkpoint.save()
 
+  final_eval_directory = logdir / "final_eval"
+  # Create directory
+  os.makedirs(final_eval_directory, exist_ok=True)
+  episode_n = 1
+
+  def final_eval_step(tran, worker):
+    eval_step.increment()
+    final_eval_replay.add(tran)
+
+  def final_on_episode(ep, worker):
+    print(f"Final evaluation episode has {len(ep['reward'])} steps.")
+    print("Episode")
+    # Save the episode to a npz file.
+    np.savez_compressed(final_eval_directory / f"ep_{episode_n}.npz", **ep)
+    episode_n += 1
+
+  final_eval_driver = embodied.Driver(env)
+  # final_eval_driver.on_step(final_eval_step)
+  final_eval_driver.on_episode(final_on_episode)
+  policy_eval = lambda *args: agent.policy(*args, mode="eval")
+
+  # Evaluate for 100 episodes and save them
+  print("Run trained agent to run 100 episodes and save them.")
+  final_eval_driver.reset()
+  final_eval_driver(policy_eval, episodes=100)
